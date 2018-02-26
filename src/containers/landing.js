66c8b009e0ec0557c510e6ab19505f64bee48b5d
TarCode <tarcode33@gmail.com>
2018-02-26 13:57:30 +0200

    replace logo and align with right content in landing page

--- src/containers/landing.js
+++ src/containers/landing.js
@@ -13,9 +13,9 @@ export default class extends Component {
 				<div className='row'>
 					<div className='col-6 center'>
 						<br/>
-						<img alt='logo' className='container' src='./logo.jpg' />
+						<img alt='logo' className='landing-img' src='./logo.png' />
 					</div>
-					<div className='col-6 center'>
+					<div className='col-6 center landing-row'>
 						<h1 className='title'>Product Name</h1>
 						<p className='subtitle'>This is a short description of the product</p>
 							<TextField
