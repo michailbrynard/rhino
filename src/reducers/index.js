569afca7685562f6112aebd4f8b6eccb3215d148
TarCode <tarcode33@gmail.com>
2018-02-28 14:02:46 +0200

    added login reducer, connect login action to landing component, call rehive login endpoint, render login error messages

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import home from './home'
 import signup from './signup'
+import login from './login'
 
 export default combineReducers({
 	home,
-	signup
+	signup,
+	login
 })
