4687ab822f4d5bd44e2a04fa365f275196d8fb15
TarCode <tarcode33@gmail.com>
2018-03-07 23:22:26 +0200

    moved stellar service username check and set to login

--- src/actions/index.js
+++ src/actions/index.js
@@ -12,35 +12,14 @@ export const signup = (signup_email) => (
 				'Content-Type': 'application/json',
 			},
 			mode: 'cors',
-			body: JSON.stringify({ signup_email, company: 'launcher_test', referal_id: "d0a009c9-b5bc-4239-93d2-2f54220076a2" })
+			body: JSON.stringify({ signup_email, company: 'launcher_test', referral_id: "d0a009c9-b5bc-4239-93d2-2f54220076a2" })
 		})
 		.then(response => {
 			return response.json()
 		})
 		.then(json => {
 			if(json.status === 'success') {
-				fetch(process.env.REACT_APP_STELLAR_SERVICE_URL + '/user/username/set', {
-					method: 'POST',
-					headers: {
-						'Accept': 'application/json',
-						'Content-Type': 'application/json',
-					},
-					mode: 'cors',
-					body: JSON.stringify({ username: signup_email })
-				})
-					.then(response => {
-						return response.json()
-					})
-					.then(json => {
-						if (json.status === 'success') {
-							dispatch({ type: SIGNUP_SUCCESS, data: json.data })
-						} else {
-							dispatch({ type: SIGNUP_ERROR, err: json.message })
-						}
-					})
-					.catch(err => {
-						dispatch({ type: SIGNUP_ERROR })
-					})
+					dispatch({ type: SIGNUP_SUCCESS, data: json.data })
 			} else {
 				dispatch({ type: SIGNUP_ERROR, err: json.message })
 			}
@@ -105,7 +84,19 @@ export const login = (user, password) => (
 			})
 			.then(json => {
 				if (json.status === 'success') {
-					dispatch({ type: LOGIN_SUCCESS, data: json.data })
+					checkStellarUsername(json.data.token)
+						.then(r => {
+							if (r.status == 'error') {
+								setStellarUsername(user)
+								.then(setRes => {
+									if (setRes.status == 'error') {
+										dispatch({ type: LOGIN_ERROR, err: "Error setting Stellar Username" })
+									} else {
+										dispatch({ type: LOGIN_SUCCESS, data: json.data })
+									}
+								})
+							}
+						})
 				} else {
 					dispatch({ type: LOGIN_ERROR, err: json.message })
 				}
@@ -248,3 +239,39 @@ export const getRewardCountData = (company, reward_type) => {
 			})
 	}
 }
+
+const checkStellarUsername = (token) => {
+	return fetch(process.env.REACT_APP_STELLAR_SERVICE_URL + '/user/account/', {
+		method: 'GET',
+		headers: {
+			'Accept': 'application/json',
+			'Content-Type': 'application/json',
+			'authorization': `Bearer ${token}`
+		},
+		mode: 'cors'
+	})
+	.then(response => {
+		return response.json()
+	})
+	.catch(err => {
+		return err
+	})
+}
+
+const setStellarUsername = (signup_email) => (
+	fetch(process.env.REACT_APP_STELLAR_SERVICE_URL + '/user/username/set/', {
+		method: 'POST',
+		headers: {
+			'Accept': 'application/json',
+			'Content-Type': 'application/json',
+		},
+		mode: 'cors',
+		body: JSON.stringify({ username: signup_email })
+	})
+	.then(response => {
+		return response.json()
+	})
+	.catch(err => {
+		return err
+	})
+)
