e935ab124541ca7ea9248d9385caa848922d433d
Michail Brynard <mbrynard@gmail.com>
2018-03-08 00:17:27 +0100

    Add stellar service url configuration.

--- etc/docker/docker-entrypoint.sh
+++ etc/docker/docker-entrypoint.sh
@@ -4,5 +4,6 @@ set -e
 
 perl -pi -e 's/\{\{API_URL\}\}/$ENV{'API_URL'}/g' /usr/share/nginx/html/static/js/*
 perl -pi -e 's/\{\{REHIVE_API_URL\}\}/$ENV{'REHIVE_API_URL'}/g' /usr/share/nginx/html/static/js/*
+perl -pi -e 's/\{\{STELLAR_SERVICE_URL\}\}/$ENV{'STELLAR_SERVICE_URL'}/g' /usr/share/nginx/html/static/js/*
 
 exec "$@"
