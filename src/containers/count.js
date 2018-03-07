505662d3102858d2d742334b14531e2693bf444f
TarCode <tarcode33@gmail.com>
2018-03-07 13:58:35 +0200

    added rewards component and created tabs for signup and reward counts

--- src/containers/count.js
+++ src/containers/count.js
@@ -14,12 +14,12 @@ import { bindActionCreators } from 'redux'
 
 
 
-class Count extends Component {
+class SignupCount extends Component {
 
 	componentDidMount() {
-		const { getSignupCountData, match } = this.props
+		const { getSignupCountData, params } = this.props
 		const user_data = JSON.parse(localStorage.getItem('user'))
-		console.log("PARAMS", match.params);
+		console.log("PARAMS", params);
 		getSignupCountData(user_data.company)
 	}
 	render() {
@@ -27,46 +27,102 @@ class Count extends Component {
 
 		return (
 			<div>
+					<div style={style.content}>
+						<div>
+						<h1 style={style.header}>108 801</h1>
+						<span>USERS SIGNED UP</span>
+						</div>
+					</div>
+					<div style={style.overlay}></div>
+				<div className='row'>
+					<div className='col-12 center'>
+						<img alt='logo' style={style.image} src='./rehive-logo1.svg' />
+					</div>
+				</div>
+			</div>
+		)
+	}
+}
+
+class RewardCount extends Component {
+
+	componentDidMount() {
+		const { getSignupCountData, params } = this.props
+		const user_data = JSON.parse(localStorage.getItem('user'))
+		console.log("PARAMS", params);
+		getSignupCountData(user_data.company)
+	}
+	render() {
+		const { history } = this.props
+
+		return (
+			<div>
+				<div style={style.content}>
+					<div>
+						<h1 style={style.header}>1108 8011</h1>
+						<span>REWARDS CLAIMED</span>
+					</div>
+				</div>
+				<div style={style.overlay}></div>
+				<div className='row'>
+					<div className='col-12 center'>
+						<img alt='logo' style={style.image} src='./rehive-logo1.svg' />
+					</div>
+				</div>
+			</div>
+		)
+	}
+}
+
+const style = {
+	content: {
+		position: 'absolute',
+		top: 0, left: 0, bottom: 0, right: 0,
+		zIndex: 11,
+		display: 'flex',
+		justifyContent: 'center',
+		textAlign: 'center',
+		alignContent: 'center',
+		alignItems: 'center',
+		color: '#fff'
+	},
+	header: {
+		fontSize: '108px',
+		margin: '5px'
+	},
+	overlay: {
+		position: 'absolute',
+		top: 0, left: 0, bottom: '-50px', right: 0,
+		backgroundColor: '#333',
+		zIndex: 10,
+		opacity: 0.9
+	},
+	image: {
+		margin: 'auto',
+		display: 'block',
+		height: '80vh',
+		width: 'auto'
+	}
+}
+
+class Count extends Component {
+	render() {
+		const { history, match, getSignupCountData } = this.props
+
+		return (
+			<div>
 				<AppBar
 					title={<img alt='logo' className='header-img' src='./logo.svg' />}
 					showMenuIconButton={false}
 				/>
-					<div style={{
-						position: 'absolute',
-						top: 0, left: 0, bottom: 0, right: 0,
-						zIndex: 11,
-						display: 'flex',
-						justifyContent: 'center',
-						textAlign: 'center',
-						alignContent: 'center',
-						alignItems: 'center',
-						color: '#fff'
-					}}>
-						<div>
-						<h1 style={{
-							fontSize: '108px',
-							margin: '5px'
-						}}>108 801</h1>
-						<span>USERS SIGNED UP</span>
-						</div>
-					</div>
-					<div style={{
-						position: 'absolute',
-						top: 0, left: 0, bottom: 0, right: 0,
-						backgroundColor: '#333',
-						zIndex: 10,
-						opacity: 0.9
-					}}></div>
-				<div className='row'>
-					<div className='col-12 center'>
-						<img alt='logo' style={{
-							margin: 'auto',
-							display: 'block',
-							height: '80vh',
-							width: 'auto'
-						}} src='./rehive-logo1.svg' />
-					</div>
-				</div>
+				<Tabs>
+					<Tab label="Signups">
+						<SignupCount getSignupCountData={getSignupCountData} params={match.params} />
+					</Tab>
+					<Tab label="Rewards Claimed">
+						<RewardCount getSignupCountData={getSignupCountData} params={match.params} />
+					</Tab>
+				</Tabs>
 			</div>
 		)
 	}
