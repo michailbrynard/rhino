b992a3dbee22e20436fce4f172d3701603934aa9
TarCode <tarcode33@gmail.com>
2018-04-12 13:22:30 +0200

    added redeemPerk action

--- src/actions/perk.js
+++ src/actions/perk.js
@@ -22,3 +22,29 @@ export const getPerkData = (company) => {
 			})
 	}
 }
+
+export const REDEEM_PERK = "REDEEM_PERK"
+export const REDEEM_PERK_SUCCESS = "REDEEM_PERK_SUCCESS"
+export const REDEEM_PERK_ERROR = "REDEEM_PERK_ERROR"
+
+export const  redeemPerk = data => (
+	dispatch => {
+		dispatch({ type: REDEEM_PERK })
+		const route = process.env.REACT_APP_API_URL + '/user/perk/buy'
+		const token = localStorage.getItem('token')
+
+		callApi('POST', route, token, data)
+			.then(json => {
+				if (json.status === 'success') {
+					window.location.reload()
+					dispatch({ type: REDEEM_PERK_SUCCESS, data: json.data })
+				} else {
+					dispatch({ type: REDEEM_PERK_ERROR, err: json.message })
+				}
+			})
+			.catch(err => {
+				dispatch({ type: REDEEM_PERK_ERROR })
+			})
+
+	}
+)
